0392ec5160f85a2d508c28861c0386b738a7fc36
devanooo <trestantodevano@gmail.com>
2026-02-20 11:18:58 +0700

    melengkapi Mtitik

--- Pertemuan1/src/Mtitik.java
+++ Pertemuan1/src/Mtitik.java
@@ -8,7 +8,10 @@
 public class Mtitik{
     public static void main(String[] args){
         Titik T1 = new Titik(); //membuat objek titik T1 (0,0)
-        
-
+        T1.setAbsis(3);
+        T1.setOrdinat(4);
+        T1.printTitik();
+        T1.geser(3, 4);
+        T1.printTitik();
     }
 } 
