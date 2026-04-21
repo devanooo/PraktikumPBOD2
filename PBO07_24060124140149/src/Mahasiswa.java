ff9688364450ac6a223014f5ac0f0352e5d81fab
devanooo <trestantodevano@gmail.com>
2026-04-21 16:32:30 +0700

    ngerjain tipis

---
+++ PBO07_24060124140149/src/Mahasiswa.java
@@ -0,0 +1,56 @@
+public class Mahasiswa {
+    String NIM;
+    String Nama;
+    String Programstudi;
+
+    Mahasiswa(){
+        NIM = "-999";
+        Nama = " ";
+        Programstudi = "n/a";
+    }
+
+    Mahasiswa(String NIM, String Nama, String Programstudi){
+        this.NIM = NIM;
+        this.Nama = Nama;
+        this.Programstudi = Programstudi;
+    }
+    public void setProgramStudi(){
+        this.Programstudi = "Kosong";
+    }
+    public void setProgramStudi(String x){
+        this.Programstudi = x;
+    }
+
+    public void setProgramStudi(Mahasiswa mhslain){
+        this.Programstudi = mhslain.Programstudi;
+    }
+
+    public void setNIM(String Nim) {
+        NIM = Nim;
+    }
+
+    public void setNama(String nama) {
+        Nama = nama;
+    }
+    
+    public void setProgramstudi(String programstudi) {
+        Programstudi = programstudi;
+    }
+
+    public String getNIM() {
+        return NIM;
+    }
+
+    public String getNama() {
+        return Nama;
+    }
+    public String getProgramstudi() {
+        return Programstudi;
+    }
+
+    public void printInfo(){
+        System.out.println("NIM: " + this.NIM);
+        System.out.println("Nama: " + this.Nama);
+        System.out.println("Program Studi: " + this.Programstudi);
+    }
+}
---
+++ PBO07_24060124140149/src/mainpolimorfisme.java
@@ -0,0 +1,49 @@
+public class mainpolimorfisme {
+    public static void main(String[] args) throws Exception {
+        Integer I = 20;
+        double B = (double) I;
+        Integer C = (int) B;
+        System.out.println("Integer I : " +I);
+        System.out.println("Convert I menjadi real : " + B);
+        System.out.println("Convert kembali menjadi Integer : " + C);
+
+        String X = "1234";
+        String Y = "5678";
+        Integer Z = Integer.parseInt(X) + Integer.parseInt(Y);
+
+        System.out.println("String X : " + X + "\n" + "String Y : " + Y);
+        System.out.println("Z : " + Z);
+        
+        String P = "12.34";
+        String Q = "56.78";
+        double D = Double.parseDouble(P) + Double.parseDouble(Q);
+        
+        System.out.println("String Q : " + Q + "\n" + "String Q : " + Q);
+        System.out.println("D : " + D);
+        
+        String S = "2023";
+        Integer A = Integer.parseInt(S);
+        System.out.println(A);
+        
+        String T = A.toString();
+        System.out.println(T);
+        
+
+        Mahasiswa M1 = new Mahasiswa();
+        Mahasiswa M2 = new Mahasiswa("1234", "Devano", "");
+        
+        System.out.println("==M1==");
+        M1.printInfo();
+        System.out.println("==M2==");
+        M2.printInfo();
+        
+        
+        System.out.println("==setelah setprogramstudi==");
+        M1.setProgramStudi(M2);
+        System.out.println("==M1==");
+        
+
+        
+
+    }
+} 
